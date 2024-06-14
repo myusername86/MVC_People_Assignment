d7b6c420667771c99b50e398a0e443bf96680cca
yuva <pyuvarani.2011@gmail.com>
2024-06-14 11:09:00 +0200

    Add project files.

---
+++ MVC_People/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_People.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string City { get; set; }
+    }
+
+
+
+
+
+}
+
+
